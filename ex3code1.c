569f630ef9b3fd0ff6c6a12377926ecdc7c2a3a9
RutanshSantoki <rutansh.santoki98@gmail.com>
2024-12-19 18:47:36 +0530

    Sem1

---
+++ ex3code1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int h;
+    printf("Enter the number");
+    scanf("%d",&h);
+
+    if(h%2==0)
+    printf("Number is even\n");
+
+    else
+     printf("Number is odd\n");
+
+     return 0;
+}
