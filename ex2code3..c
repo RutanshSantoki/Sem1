67151ff2ebcb3720f02a3b1e38561cf1a97548a4
RutanshSantoki <rutansh.santoki98@gmail.com>
2025-01-12 13:10:22 +0530

    Sem1

--- ex2code3..c
+++ ex2code3..c
@@ -2,16 +2,19 @@
 
 int main()
 {
-    int num,hrs,sec,min,n;
-    printf("Enter number of seconds :");
+    int num,hrs,min,sec;
+    int n;
+
+    printf("Enternumbers of seconds:");
     scanf("%d",&num);
 
     hrs=num/3600;
     n=num%3600;
-    min=n/60;
+    hrs=n/60;
     sec=n%60;
 
-        printf("%d hours %d min %d seconds\n",hrs,min,sec);
+    printf("%d Hours %d minutes %d seconds\n",hrs,min,sec);
 
-        return 0;
+    return 0;
+
 }
